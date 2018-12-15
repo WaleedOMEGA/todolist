5180c9a13d852e9dc9396bb8a526d1df7ccfcd57
Waleed OMEGA <waleedomega@hotmail.com>
2018-12-15 20:01:21 +0200

    todoDataService

--- src/app/todo-data.service.ts
+++ src/app/todo-data.service.ts
@@ -11,6 +11,35 @@ export class TodoDataService {
 
    }
    addToDo(todo:ToDo):TodoDataService{
-     
+     if(!todo.id){
+       todo.id=++this.lastId;
+       this.todos.push(todo);
+       return this;
+     }
+   }
+   deleteToDoById(id:number):TodoDataService{
+     this.todos=this.todos.filter(todo => todo.id!==id);
+     return this;
+   }
+   updateTodoById(id:number,values:object={}):ToDo{
+    let todo=this.getToDoById(id);
+    if(!todo){
+      return null;
+    }
+    Object.assign(todo,values);
+    return todo;
+   }
+   getAllTodos():ToDo[]{
+     return this.todos;
+   }
+   getToDoById(id:number):ToDo{
+     return this.todos.filter(todo => todo.id===id).pop();
+   }
+   toggleTodoComplete(todo:ToDo){
+     let updatedTodo=this.updateTodoById(todo.id,{
+       complete:!todo.complete
+     });
+     return updatedTodo;
    }
 }
+
